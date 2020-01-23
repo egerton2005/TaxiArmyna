eca277f2fa2c051fdc3220c294727bf4a36865b3
egerton2005 <60086857+egerton2005@users.noreply.github.com>
2020-01-23 11:53:10 +0300

    plus GyroSensor

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ from pybricks.parameters import (Port, Stop, Direction, Button, Color,
 from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 
-# Write your program here
-brick.sound.beep()
+# CONST
 leftMotor = Motor(Port.A)
 rightMotor = Motor(Port.B)
 captureMotor = Motor(Port.C)
@@ -17,40 +16,39 @@ scrollingMotor = Motor(Port.D)
 colorSensor = ColorSensor(Port.S2)
 colorSensorRight = ColorSensor(Port.S4)
 colorSensorLeft = ColorSensor(Port.S3)
-haight = UltrasonicSensor(Port.S2)
 Const_povovorot = 170
-REFLECTION_LIMIT = 10
-SPEED_FLINE = 150
-SLOW_SPEED_FLINE = 50
-Color.BLACK = 1
-Color.BLUE = 2
-Color.GREEN = 3
-Color.YELLOW = 4
-Color.RED = 5
-Color.WHITE = 6
-Color.BROWN = 7
-location = [
-False,False,False,False,False,False
-]
+Const_reflection_limit = 10
+Const_speed_fline = 150
+Const_slow_speed_fline = 50
+location = [False,False,False,False,False,False]
+gyroSensor = GyroSensor(Port.S1)
 
 
+def gyroSensorIsTrue():
+    angle = gyroSensor.angle() % 360
+    return (angle < 80 or angle > 110)
+
+#поворот на 90 градусов
+#angle - угол(градусов)
 def povorot(angle):
     leftMotor.reset_angle(0)
     leftMotor.dc(50)
     rightMotor.dc(-50)
+    print('povorot:'+angle)
     while(abs(leftMotor.angle()) < angle):
-        print(abs(leftMotor.angle()))
         leftMotor.stop
         rightMotor.stop
 
-
+#ехать вперёд
+#left,right моторы
 def motorRule(left,right):
-    motorLeft.run(left)
-    motorRight.run(right)
+    leftMotor.run(left)
+    rightMotor.run(right)
 
 #езда по черной линии
 #crossroadCounts - кол-во перекрестков, которые необходимо проехать
 def crossroad (crossroadCounts):
+    print('crossroad:'+crossroadCounts)
     crossroad = 0
     blackLine = False
     while(True):
@@ -59,19 +57,22 @@ def crossroad (crossroadCounts):
 
         fline(reflectionLEFT, reflectionRight)
         
-        if((reflectionLEFT < REFLECTION_LIMIT) & (reflectionRight < REFLECTION_LIMIT)):
+        if((reflectionLEFT < Const_reflection_limit) & (reflectionRight < Const_reflection_limit)):
             if(blackLine == False):
                 crossroad = crossroad + 1
-            if(crossroad == crossroadCounts):
-                motorRule(0,0)
-                break
+            
             blackLine = True
         else:
             blackLine = False
+            if(crossroad == crossroadCounts):
+                motorRule(0,0)
+                break
 
 
-
+#readingОbstacles считывание препятствий
+#obstacleCounts кол-во препятствий 
 def readingОbstacles (obstacleCounts):
+    print('readingОbstacles:'+str(obstacleCounts))
     obstacle = 0   
     readingОbstacles = False
     while(True):
@@ -79,55 +80,67 @@ def readingОbstacles (obstacleCounts):
         reflectionRight = colorSensorRight.reflection()
 
         fline(reflectionLEFT, reflectionRight)
-        
-        if (colorSensor != None):
-            if(readingОbstacles == False)
+
+        print(gyroSensorIsTrue())
+        if (colorSensor.color() != None and gyroSensorIsTrue()):
+            if(readingОbstacles == False):
                 obstacle = obstacle + 1
             readingОbstacles = True
+
+            if(obstacle >= obstacleCounts):
+                break
         else:
             readingОbstacles == False
-        if(obstacle >= obstacleCounts):
-            break
 
 def fline(reflectionLEFT, reflectionRight):
-    if(reflectionLEFT < REFLECTION_LIMIT):
-            if(reflectionRight < REFLECTION_LIMIT):
-                motorRule(SPEED_FLINE,SPEED_FLINE)
-            else:
-                motorRule(-SLOW_SPEED_FLINE,SLOW_SPEED_FLINE)
+    if(reflectionLEFT < Const_reflection_limit):
+        if(reflectionRight < Const_reflection_limit):
+            motorRule(Const_speed_fline,Const_speed_fline)
         else:
-            if(reflectionRight < REFLECTION_LIMIT):
-                motorRule(SLOW_SPEED_FLINE,-SLOW_SPEED_FLINE)
-            else:
-                motorRule(SPEED_FLINE,SPEED_FLINE)
+            motorRule(-Const_slow_speed_fline,Const_slow_speed_fline)
+    else:
+        if(reflectionRight < Const_reflection_limit):
+            motorRule(Const_slow_speed_fline,-Const_slow_speed_fline)
+        else:
+            motorRule(Const_speed_fline,Const_speed_fline)
 
 
-def thisColor(colorSensor):
-    if colorSensor.Color() != None:
-        color = colorSensor.Color()
+def thisColor():
+    if colorSensor.color() != None:
+        color = colorSensor.color()
+    else:
+        print('не увидел цвет')
     return color
 
 def distributor():
     step = 0
     while(0<6):
-        readingОbstacles(1)#Проехал до кубика
-        colorFirst = thisColor()#считал цвет кубика
+        readingОbstacles(1) #Проехал до кубика
+        colorFirst = thisColor() #считал цвет кубика
+        print('colorFirst:'+str(colorFirst))
+        print('Взял:')
+
         #прокрутка вниз
         #взять
         #прокрутка вверх
         readingОbstacles(5 - step + 1)
-        for( x in range(0,6)):
+        for x in range(0,6):
             colorSecond = thisColor()
+            print('colorSecond:'+colorSecond)
             if(colorFirst==colorSecond & location[x] == False):
                 #прокрутка вниз
                 #поставить
                 #прокрутка вверх
                 location[x] == True
+                print('Поставил:')
                 readingОbstacles(5 - x)
+                step = step + 1
                 break
             else:
                 readingОbstacles(1)
-            
 
 
 
+
+gyroSensor.reset_angle(0)
+distributor()
